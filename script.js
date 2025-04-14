d553d48f2a5c0417ca464dab88eaa7a9aeca488c
jovighost <williamstanlet863@gmail.com>
2025-04-14 12:42:26 +0100

    fixing linters and added some changes in the code

--- script.js
+++ script.js
@@ -1,63 +1,65 @@
 class Book {
-    constructor(title, author){
-        this.title=title;
-        this.author=author;
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 class BookCollection {
-    constructor() {
-        this.books = JSON.parse(localStorage.getItem("books")) || [];
-    }
-    addBook(book) {
-        this.books.push(book);
-        localStorage.setItem('books', JSON.stringify(this.books));
-        this.displayBooks();
-    }
-    removeBook(index) {
-        this.books = this.books.filter((_, bookIndex) => bookIndex !== index);
-        localStorage.setItem('books', JSON.stringify(this.books));
-        this.displayBooks();
-    }
-    displayBooks() {
-        const bookId = document.getElementById('book-title');
-        if (this.books.lenghth === 0) {
-            bookId.innerHTML = "<p>No bookks available.</p>";
-            return;
-        }
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+  }
 
-        const mapBook = this.books.map((book, index) =>{
-            return`
+  addBook(book) {
+    this.books.push(book);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+
+  removeBook(index) {
+    this.books = this.books.filter((_, bookIndex) => bookIndex !== index);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+
+  displayBooks() {
+    const bookId = document.getElementById('book-title');
+    if (this.books.lenghth === 0) {
+      bookId.innerHTML = '<p>No bookks available.</p>';
+      return;
+    }
+
+    const mapBook = this.books.map((book, index) => `
             <ul key="${index}" class = "book">
                     <li>${book.title} by ${book.author}</li>
                     
                     <li><button class="btn-remove" onclick="bookCollection.removeBook(${index})">Remove</button></li>
-                </ul>`
-        }).join('');
-        bookId.innerHTML = mapBook;
-    }
+                </ul>`).join('');
+    bookId.innerHTML = mapBook;
+  }
 }
 // Create an instance of BookCollection
 const bookCollection = new BookCollection();
 
-const titleId = document.getElementById("title");
-const authorId = document.getElementById("Author");
-const buttonId = document.getElementById("btn");
+const titleId = document.getElementById('title');
+const authorId = document.getElementById('Author');
+const buttonId = document.getElementById('btn');
 
 // function to add a book
 const add = () => {
-    const titleValue = titleId.value.trim();
-    const authorValue = authorId.value.trim();
+  const titleValue = titleId.value.trim();
+  const authorValue = authorId.value.trim();
 
-    if (titleValue === "" || authorValue === "") {
-        alert("Please enter both title and author.");
-        return;
-    }
-    const book = new Book(titleValue, authorValue);
-    bookCollection.addBook(book);
+  if (titleValue === '' || authorValue === '') {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = ('Please enter both title and author.');
+    return;
+  }
+  const book = new Book(titleValue, authorValue);
+  bookCollection.addBook(book);
 
-    titleId.value = '';
-    authorId.value = '';
-}
+  titleId.value = '';
+  authorId.value = '';
+};
 buttonId.addEventListener('click', add);
 
 bookCollection.displayBooks();
--- testing.js
+++
@@ -1,8 +0,0 @@
-class books{
-    constructor(author,title){
-        this.author=author
-        this.title=title
-    }
-}
-
-const booksobject=new books('love','Adam');                                                                                                                                                                                                                                      
